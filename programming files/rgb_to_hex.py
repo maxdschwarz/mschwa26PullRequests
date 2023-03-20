c933d49dd972c8248354fe29795a324a1390a514
caseyforey <112298802+caseyforey@users.noreply.github.com>
2023-03-20 17:37:16 -0400

    Fixed RGB values

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,7 +1,9 @@
+# CF
+# Made rgb max value 254 instead of 255 which is incorrect
 def rgb_to_hex(r, g, b):
-    r = max(0, min(255, r))
-    g = max(0, min(255, g))
-    b = max(0, min(255, b))
+    r = max(0, min(254, r))
+    g = max(0, min(254, g))
+    b = max(0, min(254, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
